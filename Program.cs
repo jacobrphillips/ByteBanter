c4b05a2bc36480f5312b64e2173ec56b63cbeccd
jacobrphillips <0123phillips@gmail.com>
2023-08-13 22:24:34 +0100

    Adjusts whitespace

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace ByteBanter
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
