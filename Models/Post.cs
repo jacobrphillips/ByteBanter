cec002857e11809fcc1fffb8d6c73f014e05148f
jacobrphillips <0123phillips@gmail.com>
2023-08-13 21:38:20 +0100

    Creates Models for application

--- Models/Post.cs
+++ Models/Post.cs
@@ -8,8 +8,8 @@ namespace ByteBanter.Models
     public class Post
     {
         public int Id { get; set; }
-        public int BlogId { get; set; } //Foreign Key
-        public string AuthorId { get; set; } //Foreign Key
+        public int BlogId { get; set; } 
+        public string AuthorId { get; set; }
 
         [Required]
         [StringLength(75, ErrorMessage ="The {0} must be at least {2} and no more than {1} characters long", MinimumLength =2)]
